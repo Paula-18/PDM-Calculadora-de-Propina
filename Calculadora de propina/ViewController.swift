f5b21983279383fa8a5038687386c16b773e9795
analourdescr <ana_lourdes98@hotmail.com>
2021-08-31 16:09:39 -0700

    Initial Commit

---
+++ Calculadora de propina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculadora de propina
+//
+//  Created by Alumno on 8/31/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
